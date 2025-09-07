09e1e58561eb3332bd40094ac607bfb8ea9595cc
Naufal Weise <naufal.weise@ui.ac.id>
2025-09-07 15:36:24 +0700

    Add sign-up

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-
+from . import views
 
 urlpatterns = [
    path("", include("django.contrib.auth.urls")),
+   path("sign-up", views.view_sign_up),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 # Create your views here.
+
+def view_sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            # return form with error message
+            return render(request, 'registration/sign-up.html', {'form': form})
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'registration/sign-up.html', context)
+
+class SignUpView(CreateView):
+    template_name = "registration/sign-up.html"
+    form_class = UserCreationForm
