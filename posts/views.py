ad7dcdcf9257ea54bec71593892d148fe0be2ee7
Naufal Weise <naufal.weise@ui.ac.id>
2025-09-20 21:55:30 +0700

    Add create post

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # all post list, homepage
 def all_post_list(request):
     posts = Post.objects.all()
-    return render(request, "posts/all-post-list.html", {'posts': posts})
+    form = PostForm()
+    return render(request, "posts/all-post-list.html", {'posts': posts, 'post_form': form})
 
 def create_post(request):
-    pass
-    #return render(request, "post")
+    form = PostForm(request.POST)
+    if form.is_valid():
+        new_post = form.save(commit=False)
+        new_post.author = request.user
+        new_post.save()
+        return redirect('/posts/')
+    
+    # TODO: add error handling
+    return redirect('/posts')
