3c6d4833b5b918e788ef699c57bae710b60da9d5
Naufal Weise <naufal.weise@ui.ac.id>
2025-10-09 16:15:05 +0700

    Add post media

--- posts/forms.py
+++ posts/forms.py
@@ -2,7 +2,25 @@ from django import forms
 from django.forms import ModelForm
 from .models import Post
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
 class PostForm(ModelForm):
+    media_files = MultipleFileField()
     class Meta:
         model = Post
         fields = ["content"]
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 
@@ -7,3 +8,11 @@ class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PostMedia(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    file = models.FileField(
+        upload_to="uploads/",
+        validators=[FileExtensionValidator(allowed_extensions=["mp4", "jpeg"])]
+    )
